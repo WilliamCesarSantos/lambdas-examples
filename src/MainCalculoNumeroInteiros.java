4c1d5ba2c5be186e5b9351b369017892b9ca4ff9
William Cesar Santos <william_cesar_santos@hotmail.com>
2024-10-07 21:48:27 -0300

    updat

---
+++ src/MainCalculoNumeroInteiros.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class MainCalculoNumeroInteiros {
+
+    public static void main(String[] args) {
+        //- Dado uma lista de número inteiros, calcule a soma total dos números.
+        List<Integer> numeros = Arrays.asList(1, 5, 69, 8, 95, 16, 456, 489, 65, 13, 85, 87, 78, 4, 101);
+    }
+}
